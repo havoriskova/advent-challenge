0bd8c8d1c3c5db108cca46f6347a5e594cb66acd
havoriskova <ha.voriskova@gmail.com>
2023-09-07 22:07:22 +0200

    Update Day2.js

--- src/year2021/Day2.js
+++ src/year2021/Day2.js
@@ -1,9 +1,65 @@
 
 
 function Day2() {
+
+  const inputDay2 = 'forward 1\ndown 5\ndown 6\ndown 2\nforward 8\nup 3\nup 2\ndown 2\nforward 9\nforward 7\ndown 2\nforward 5\ndown 2\nforward 2\nforward 1\ndown 8\nforward 5\ndown 6\nup 9\ndown 7\nforward 6\nforward 7\nforward 8\nup 3\ndown 8\nforward 9\nforward 5\nforward 2\ndown 3\ndown 3\ndown 6\ndown 6\ndown 8\ndown 4\nforward 1\nup 9\ndown 7\nup 6\ndown 2\ndown 8\nforward 1\nforward 7\nforward 4\nforward 1\nforward 7\ndown 1\ndown 9\nforward 6\nforward 6\nforward 7\nforward 3\nup 7\nforward 7\nup 1\nforward 3\nforward 4\nup 4\nforward 6\ndown 8\ndown 7\nforward 4\nup 6\nup 8\ndown 5\ndown 6\nforward 2\nforward 7\nforward 5\nforward 4\ndown 5\ndown 8\ndown 4\ndown 4\ndown 3\nforward 3\ndown 8\nforward 7\nup 6\ndown 3\ndown 7\ndown 3\nforward 2\nup 8\ndown 7\ndown 6\ndown 3\nup 5\nup 4\ndown 2\ndown 5\nup 9\ndown 8\nup 7\nup 8\nforward 7\ndown 9\nup 5\ndown 5\ndown 5\nup 4\ndown 4\nup 3\ndown 1\ndown 4\ndown 7\nforward 2\nforward 9\nup 8\ndown 8\nforward 6\nforward 8\ndown 6\ndown 2\nforward 2\nforward 7\nup 7\ndown 4\nforward 1\ndown 4\nforward 2\nup 3\nforward 8\nforward 7\nforward 8\ndown 2\ndown 9\ndown 5\nforward 7\ndown 7\nforward 8\nforward 2\nup 2\nforward 9\ndown 3\nup 7\nforward 2\ndown 4\nup 3\nup 8\nforward 4\nforward 4\nforward 3\nup 3\nforward 5\nforward 5\ndown 9\ndown 4\nup 2\ndown 8\ndown 5\ndown 8\nup 7\ndown 3\ndown 7\nup 9\ndown 7\nup 5\ndown 5\nforward 2\nforward 2\nforward 6\ndown 4\nup 3\nforward 1\nforward 3\nforward 4\ndown 2\nforward 2\ndown 1\ndown 2\ndown 3\nforward 8\nforward 1\ndown 5\nforward 3\nup 2\ndown 2\nforward 3\nforward 1\nup 4\nup 1\nup 2\nforward 4\nforward 8\ndown 7\nforward 2\ndown 8\nforward 6\nforward 3\nup 7\ndown 3\ndown 3\nforward 7\nforward 9\ndown 8\ndown 8\nup 1\nup 8\ndown 5\nup 3\nforward 6\nforward 4\nforward 8\nup 4\ndown 6\nforward 2\nforward 8\ndown 1\nup 5\ndown 4\nforward 5\nup 1\nforward 2\ndown 9\ndown 2\nforward 8\nup 3\nup 7\nforward 3\nforward 4\ndown 7\nup 3\ndown 7\nforward 7\nup 1\ndown 3\ndown 5\ndown 7\nforward 5\ndown 1\nup 9\nforward 1\ndown 6\nup 4\ndown 1\nforward 7\nforward 2\ndown 2\nforward 2\ndown 7\ndown 4\ndown 7\ndown 9\ndown 6\ndown 2\nup 6\nforward 8\ndown 2\ndown 1\ndown 8\ndown 8\ndown 1\ndown 7\nforward 4\nforward 1\ndown 2\ndown 1\nup 2\ndown 5\nforward 3\nforward 7\nforward 9\nup 5\ndown 7\ndown 3\nforward 3\ndown 4\nforward 6\nforward 1\ndown 6\nup 5\nup 6\ndown 9\nforward 9\nforward 5\ndown 3\nforward 5\nup 4\ndown 4\ndown 6\nforward 2\nforward 2\ndown 5\ndown 1\nforward 3\nforward 5\nup 5\ndown 1\ndown 5\ndown 3\ndown 4\ndown 2\ndown 2\nforward 5\ndown 1\ndown 4\ndown 6\nforward 4\ndown 1\ndown 1\ndown 7\nforward 1\ndown 5\nforward 6\nforward 7\nforward 3\ndown 6\nforward 8\ndown 8\ndown 3\nup 9\nforward 3\nup 5\nforward 9\nforward 3\nforward 5\ndown 3\ndown 7\ndown 5\nup 5\ndown 1\nup 5\ndown 5\ndown 9\ndown 4\ndown 2\ndown 2\nup 2\nup 1\nforward 2\nforward 4\nup 5\ndown 1\nforward 6\nup 7\nup 5\ndown 5\nforward 4\ndown 1\nup 4\ndown 2\nforward 3\nforward 4\ndown 4\ndown 8\ndown 4\nup 3\nforward 6\nforward 3\nforward 1\nup 3\nforward 6\ndown 1\nup 5\ndown 5\ndown 9\ndown 9\ndown 4\nforward 9\ndown 6\ndown 6\nforward 2\nup 7\ndown 4\ndown 4\ndown 3\nup 1\ndown 4\nup 2\nforward 9\ndown 2\ndown 6\ndown 6\nup 5\ndown 7\ndown 6\ndown 9\ndown 8\nforward 5\ndown 6\nup 3\ndown 8\nup 3\nforward 5\ndown 1\nforward 4\nforward 3\ndown 9\ndown 4\nforward 8\nforward 8\nup 4\nforward 7\nup 5\ndown 9\ndown 2\nforward 6\ndown 4\nforward 9\nup 1\ndown 7\ndown 3\nforward 6\nup 7\nforward 2\nforward 1\ndown 9\nup 4\nforward 2\nforward 6\nforward 9\nforward 9\ndown 7\nforward 4\ndown 2\nforward 1\nforward 8\ndown 7\ndown 1\nforward 5\nforward 5\ndown 8\nup 8\nup 1\nforward 2\ndown 8\nforward 6\nforward 1\nforward 2\nforward 7\ndown 3\nforward 5\nup 6\nup 8\ndown 7\nforward 2\nforward 5\ndown 5\nforward 8\ndown 6\ndown 3\nup 2\ndown 7\nforward 5\ndown 4\nforward 3\nup 7\nup 6\nforward 4\nforward 7\nforward 6\ndown 4\nforward 5\nup 3\nup 5\ndown 8\nforward 7\nforward 2\nforward 3\nup 4\ndown 1\nup 2\nup 1\nforward 6\nup 3\ndown 3\nforward 7\ndown 6\nforward 9\nforward 9\ndown 7\nforward 1\ndown 9\ndown 9\nforward 8\nup 9\nup 7\nup 4\nforward 4\ndown 3\ndown 4\nforward 4\nforward 5\ndown 2\ndown 7\ndown 1\nforward 7\ndown 6\ndown 6\nforward 3\nup 1\nforward 4\ndown 5\nup 7\nup 3\nup 9\nforward 8\nup 4\nforward 2\ndown 2\ndown 2\nup 8\nforward 1\nforward 3\nforward 7\ndown 7\nup 5\ndown 3\ndown 4\ndown 6\nforward 5\ndown 1\nforward 5\ndown 4\nforward 8\ndown 4\nforward 6\nforward 9\ndown 5\nup 2\ndown 2\nforward 1\nforward 5\ndown 1\ndown 4\ndown 4\nup 6\nforward 7\nforward 2\nforward 9\ndown 1\nforward 2\nforward 6\nforward 1\nup 9\ndown 7\ndown 8\nforward 3\nforward 5\ndown 4\nforward 2\nforward 2\nup 8\nforward 3\nforward 8\nforward 5\nup 6\nforward 6\nforward 3\ndown 4\nup 8\ndown 3\nforward 4\nforward 3\nup 4\nforward 7\ndown 9\nforward 3\ndown 6\nforward 9\ndown 8\nforward 6\ndown 7\ndown 1\nforward 9\ndown 4\nforward 8\nforward 5\nforward 7\nup 5\nup 3\ndown 8\ndown 5\ndown 2\nup 3\nup 7\ndown 8\nforward 6\nforward 4\nforward 9\nforward 6\nforward 4\nforward 1\ndown 1\ndown 3\nup 3\nup 7\ndown 9\ndown 5\nforward 1\nforward 6\nforward 5\ndown 9\nforward 9\nforward 5\nup 5\nforward 8\nforward 6\nforward 4\ndown 4\ndown 6\ndown 1\nforward 1\ndown 6\nforward 8\ndown 3\ndown 6\ndown 5\nforward 3\ndown 3\ndown 8\nup 6\nforward 6\ndown 6\ndown 4\ndown 8\ndown 9\ndown 3\ndown 4\ndown 5\nforward 4\ndown 7\ndown 8\nup 4\nforward 4\ndown 9\nforward 1\nforward 1\ndown 6\nup 3\nup 6\ndown 8\nup 7\nforward 3\ndown 6\ndown 1\ndown 4\nup 4\ndown 9\nforward 1\ndown 3\nup 1\nforward 8\nup 4\nup 6\nup 4\ndown 4\nup 5\nup 3\ndown 1\ndown 6\nforward 9\ndown 2\nforward 9\nforward 8\ndown 2\nforward 8\nup 5\nforward 5\nforward 5\nforward 4\nup 8\ndown 3\nforward 3\ndown 4\nup 1\nforward 4\nup 7\ndown 1\ndown 4\nforward 1\nforward 9\ndown 7\nforward 4\ndown 5\ndown 7\nforward 9\ndown 2\ndown 2\ndown 1\ndown 2\nforward 8\ndown 6\nforward 5\ndown 2\ndown 1\nup 4\nforward 7\ndown 1\ndown 5\ndown 7\ndown 6\ndown 6\nup 8\nforward 3\nup 8\nup 9\ndown 2\ndown 1\ndown 5\nup 8\nforward 4\nforward 6\ndown 2\ndown 3\nforward 2\nforward 2\nforward 2\nforward 3\ndown 7\nforward 3\ndown 4\nup 1\ndown 2\ndown 9\nup 4\ndown 8\nup 7\ndown 6\ndown 7\ndown 6\nforward 6\ndown 4\nforward 2\ndown 7\ndown 1\nforward 2\nforward 5\nforward 6\ndown 1\ndown 4\nforward 2\ndown 4\ndown 6\ndown 8\nforward 9\ndown 8\nforward 5\ndown 7\nforward 5\nup 1\ndown 1\ndown 9\ndown 3\nforward 3\nforward 5\ndown 6\nforward 6\nup 3\ndown 4\ndown 4\nforward 7\nforward 3\ndown 5\ndown 3\nforward 7\nup 5\nforward 1\ndown 6\nforward 1\nforward 9\nforward 9\nup 9\nforward 5\nup 5\nforward 6\ndown 8\nup 4\nup 8\nforward 9\nforward 4\ndown 5\ndown 2\ndown 9\nforward 3\nforward 6\nforward 2\nup 4\ndown 4\nforward 3\ndown 8\ndown 3\nup 3\nforward 8\ndown 4\nforward 4\nforward 4\nup 9\ndown 1\nup 5\nup 5\nforward 8\nforward 4\nforward 5\nforward 1\nforward 3\nforward 8\ndown 1\nforward 2\nforward 7\ndown 4\ndown 3\ndown 4\nup 7\nforward 9\nforward 7\ndown 8\nforward 2\ndown 4\nup 9\ndown 9\ndown 9\nup 2\ndown 9\ndown 9\ndown 6\nup 9\nup 6\ndown 7\nforward 3\nforward 9\nforward 2\ndown 4\nup 4\nforward 1\nup 1\ndown 6\ndown 3\ndown 2\nup 6\ndown 5\ndown 8\nup 4\nforward 1\ndown 1\ndown 4\ndown 8\ndown 5\nforward 2\ndown 5\nforward 3\nforward 1\nforward 9\nforward 4\ndown 6\ndown 4\ndown 3\nforward 5\nforward 4\ndown 1\nup 7\ndown 4\nup 3\nforward 2\nforward 4\nup 2\nup 9\nforward 4\ndown 5\nforward 9\nup 1\nforward 8\ndown 9\ndown 6\ndown 6\ndown 2\ndown 2\ndown 3\nforward 8\ndown 2\ndown 5\nforward 4\nup 3\ndown 4\nup 6\nforward 7\nup 2\ndown 6\ndown 8\nforward 1\ndown 8\ndown 9\ndown 2\nforward 3\nforward 9\nup 7\ndown 1\ndown 5\nup 7\ndown 5\nup 8\nup 4\ndown 6\nforward 1\nforward 6\nforward 9\nforward 6\nforward 6\nforward 9\nforward 8\ndown 8\ndown 8\ndown 4\ndown 7\ndown 9\nup 9\ndown 7\ndown 2\nforward 2\ndown 5\ndown 3\ndown 1\ndown 4\ndown 2\ndown 1\nup 2\nforward 6\ndown 3\ndown 3\ndown 1\nforward 8\ndown 3\nforward 1\nforward 3\ndown 1\nup 7\ndown 9\nforward 5\nforward 8\nup 7\ndown 4\ndown 3\ndown 1\nforward 2\nforward 9\nup 5\ndown 2\nforward 6\nforward 6\nforward 2\nforward 1\nforward 1\ndown 9\nforward 6\nforward 6\ndown 2\ndown 6\nup 5\nforward 5\nup 4\nforward 4\ndown 2\ndown 2\ndown 7\ndown 1\ndown 4\nforward 3\nforward 3\nup 8\ndown 8\nforward 5\nup 4\nup 9\nup 6\nup 6\ndown 7\nforward 2\nup 1\nforward 2\nforward 4\nforward 4\nforward 2\nforward 4\ndown 6\ndown 3\nforward 9\nforward 3\ndown 2\nforward 6\nforward 6\nforward 4\nforward 8\nup 1\ndown 1\nforward 1\ndown 7\nup 6\nup 8\ndown 1\ndown 4\ndown 3\nup 4\nforward 4\nforward 5\ndown 5\ndown 8\nup 7\ndown 2\ndown 6\nforward 5\n';
+  const directionsArr = inputDay2.split('\n');
+  console.log(directionsArr);
+
+  let result1 = 0;
+  let result2 = 0;
+
+  function day2Part1() {
+
+    let x = 0;
+    let y = 0;
+
+    directionsArr.forEach((str) => {
+      if (str.includes('forward')) {
+       x += parseInt(str[str.length -1]);
+      } else if (str.includes('down')){
+       y += parseInt(str[str.length -1]);
+      } else if (str.includes('up')) {
+        y -= parseInt(str[str.length -1]);
+      }
+    })
+
+    result1 = x * y;
+
+  }
+
+  function day2Part2() {
+
+    let x = 0;
+    let y = 0;
+    let aim = 0;
+
+    directionsArr.forEach((str) => {
+      if (str.includes('forward')) {
+       x += parseInt(str[str.length -1]);
+       y += aim*parseInt(str[str.length-1]);
+      } else if (str.includes('down')){
+       aim += parseInt(str[str.length -1]);
+      } else if (str.includes('up')) {
+        aim -= parseInt(str[str.length -1]);
+      }
+    })
+
+    result2 = x * y;
+    
+  }
+
+  day2Part1();
+  day2Part2();
+
   return (
-    <div>
-        <div>day 2</div>
+    <div className="container--day">
+        <h3>Day 2: Dive!</h3>
+        <div>
+          <p className="day2--puzzle-input"></p>
+          <p>Result of part one: {result1}</p>
+          <p>Result of part two: {result2}</p>
+        </div>
     </div>
   );
 }
