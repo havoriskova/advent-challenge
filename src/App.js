f373ab8c37df372e2621539b26d119247ef381cd
havoriskova <ha.voriskova@gmail.com>
2023-09-07 22:38:07 +0200

    working on day 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import {NavLink, Route, BrowserRouter, Routes} from 'react-router-dom';
 import Year2021Day1 from './year2021/Day1.js';
 import Year2021Day2 from './year2021/Day2.js';
+import Year2021Day3 from './year2021/Day3.js';
+import Year2021Day4 from './year2021/Day4.js';
+
 import Year2022Day1 from './year2022/Day1.js';
 import Year2022Day2 from './year2022/Day2.js';
 import './App.css';
@@ -21,6 +24,8 @@ function App() {
             <ul className='App-list'>
               <li className='App-list-item'><NavLink to='/2021/day1' className='App-link'>day 1</NavLink></li>
               <li className='App-list-item'><NavLink to='/2021/day2' className='App-link'>day 2</NavLink></li>
+              <li className='App-list-item'><NavLink to='/2021/day3' className='App-link'>day 3</NavLink></li>
+              <li className='App-list-item'><NavLink to='/2021/day4' className='App-link'>day 4</NavLink></li>
             </ul>
           </li>
           <li><span>Challenges from 2022:</span>
@@ -38,6 +43,8 @@ function App() {
         {/* 2021 */}
         <Route exact path='/2021/day1' element={<Year2021Day1 />} />
         <Route exact path='/2021/day2' element={<Year2021Day2 />} />
+        <Route exact path='/2021/day3' element={<Year2021Day3 />} />
+        <Route exact path='/2021/day4' element={<Year2021Day4 />} />
         
 
         {/* 2022 */}
---
+++ src/year2021/Day4.js
@@ -0,0 +1,38 @@
+
+
+function Day4() {
+
+    const inputDay4 = '';
+  
+    let result1 = 0;
+    let result2 = 0;
+  
+    function day4Part1() {
+  
+ 
+  
+    }
+  
+    function day4Part2() {
+  
+    
+      
+    }
+  
+    day4Part1();
+    day4Part2();
+  
+    return (
+      <div className="container--day">
+          <h3>Day 4: ...</h3>
+          <div>
+            <p className="day4--puzzle-input"></p>
+            <p>Result of part one: {result1}</p>
+            <p>Result of part two: {result2}</p>
+          </div>
+      </div>
+    );
+  }
+  
+  export default Day4;
+  
